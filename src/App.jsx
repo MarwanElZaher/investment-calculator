15bb5ef8cf853432f4b52fb2724d789ebc85c8fb
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-01-08 00:03:51 +0200

    app component including fields and img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import UserInputField from './components/UserInputField';
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <div id="header">
+        <img
+          src="investment-calculator-logo.png"
+          alt="investment-calculator-logo"
+        />
+        <h1>Investment Calculator</h1>
+      </div>
+      <div id="user-input">
+        <UserInputField label={'initial Investment'} />
+        <UserInputField label={'annual Investment'} />
+        <UserInputField label={'expected Return'} />
+        <UserInputField label={'duration'} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
