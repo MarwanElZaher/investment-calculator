9b5a792d0d832f70907bd27a313890bab992aeae
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-01-09 22:29:07 +0200

    created a header component and added it in the index.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import { useState } from 'react';
-import UserInputField from './components/UserInputField';
+import Header from './components/Header'
 
 function App() {
-  const [value, setValue] = useState(null);
-  const handleInputValue = (event, label) => {
-    let newValue = event.target.value;
-    setValue((prevValue) => {
-      const values = { ...prevValue, [label]: newValue };
-      return values;
-    });
-    console.log(event.target.value, label);
-  };
-  console.log(value);
+
   return (
-    <>
-      <div id="header">
-        <img
-          src="investment-calculator-logo.png"
-          alt="investment-calculator-logo"
-        />
-        <h1>Investment Calculator</h1>
-      </div>
-      <div id="user-input">
-        <UserInputField
-          onValueChange={handleInputValue}
-          label={'initial Investment'}
-        />
-        <UserInputField
-          onValueChange={handleInputValue}
-          label={'annual Investment'}
-        />
-        <UserInputField
-          onValueChange={handleInputValue}
-          label={'expected Return'}
-        />
-        <UserInputField onValueChange={handleInputValue} label={'duration'} />
-      </div>
-    </>
-  );
+    <><Header /></>)
+
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import UserInputField from "./UserInputField"
+
+const Header = () => {
+  const [value, setValue] = useState(null);
+  const handleInputValue = (event, label) => {
+    let newValue = event.target.value;
+    setValue((prevValue) => {
+      const values = { ...prevValue, [label]: newValue };
+      return values;
+    });
+    console.log(event.target.value, label);
+  };
+  console.log(value);
+  return (
+    <>
+      <div id="header">
+        <img
+          src="investment-calculator-logo.png"
+          alt="investment-calculator-logo"
+        />
+        <h1>Investment Calculator</h1>
+      </div>
+      <div id="user-input">
+        <UserInputField
+          onValueChange={handleInputValue}
+          label={'initial Investment'}
+        />
+        <UserInputField
+          onValueChange={handleInputValue}
+          label={'annual Investment'}
+        />
+        <UserInputField
+          onValueChange={handleInputValue}
+          label={'expected Return'}
+        />
+        <UserInputField onValueChange={handleInputValue} label={'duration'} />
+      </div>
+    </>
+  )}
+export default Header
