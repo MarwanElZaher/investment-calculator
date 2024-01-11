9d2e7eaebd0435cda203b4e609d38ef353e0824e
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-01-11 16:43:08 +0200

    feat: added a feature which is a new component showing table that contain investment valuse interest and capitals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Header from './components/Header.jsx'
+import Results from './components/Results.jsx';
 import UserInputFields from './components/UserInputFields.jsx'
+import { useState } from "react";
 function App() {
-
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  })
+  const inputIsValid = userInput.duration >= 1 
+  const onValueChange = (label, event) => {
+    let newValue = event.target.value
+    let parsedValue= parseFloat(newValue)
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+      [label]:parsedValue
+      }
+    })
+  }
   return (
     <>
       <Header />
-      <UserInputFields/>
+      <UserInputFields onChangeInput={onValueChange} userInput={userInput} />
+      {inputIsValid ? <Results userInput={userInput} /> : <div style={{ display: "flex", justifyContent: "center" }} >
+        <h4>Please Enter a Valid Input Data.<br/><p style={{ color:"red"}}> Note: Duration Field should be greater than zero</p> </h4>
+        </div>
+      }
     </>)
 }
 
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,37 @@
-const Results = ({userInput}) => {
-    return (<p>{ console.log(userInput,"userInput")}</p>)
+import { calculateInvestmentResults,formatter } from "../util/investment"
+
+const Results = ({ userInput }) => {
+    const results = calculateInvestmentResults(userInput);
+    console.log(results, "results")
+    const initialInvestment = results[0]?.valueEndOfYear - results[0]?.interest - results[0]?.annualInvestment;
+    
+    return (
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Values</th>
+                    <th>Interest earned (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capitals</th>
+                </tr>
+              
+            </thead>
+            <tbody>{results.map((yearResult) => {
+                const totalInterest = ((yearResult.valueEndOfYear - yearResult.annualInvestment) * yearResult.year) - initialInvestment
+                const totalAmountInvested = yearResult?.valueEndOfYear-totalInterest
+                return <tr key={yearResult.year}>
+                    <td>{yearResult.year}</td>
+                    <td>{formatter.format(yearResult.valueEndOfYear)}</td>
+                    <td>{formatter.format(yearResult.interest)}</td>   
+                    <td>{formatter.format(totalInterest)}</td>
+                    <td>{formatter.format(totalAmountInvested)}</td>
+                   
+                    
+                </tr>}
+                
+            )}
+            </tbody>
+        </table>)
 }
 export default Results
